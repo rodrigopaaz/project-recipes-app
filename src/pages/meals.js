174cc604cb9ebc513a0bb2dd57e8ba9cc7d95cf9
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-11-29 22:55:30 -0300

    [REQUISITO 8] Feat: Concluido

--- src/pages/meals.js
+++ src/pages/meals.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
@@ -8,7 +9,13 @@ export default function Meals() {
     <div>
       <Header>
         <h4 data-testid="page-title">Meals</h4>
-        <img src={ profileIcon } alt="profile-icon" data-testid="profile-top-btn" />
+        <Link to="/profile">
+          <img
+            src={ profileIcon }
+            alt="profile-icon"
+            data-testid="profile-top-btn"
+          />
+        </Link>
         <img src={ searchIcon } alt="search-icon" data-testid="search-top-btn" />
       </Header>
     </div>
