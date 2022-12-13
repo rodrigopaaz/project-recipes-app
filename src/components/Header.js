0052836c592bea4bcec5f848ee2a8789cdee472e
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-12-13 19:29:42 -0300

    criando carrosel

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import AppContext from '../context/Context';
 import SearchBar from './SearchBar';
-import headerRecipesImg from '../images/header_recipes_img.png';
-import logoRecipesApp from '../images/logo Recipes app.png';
+import headerRecipesImg from '../images/header_recipes_img.svg';
+import logoRecipesApp from '../images/logo Recipes app.svg';
 import meals from '../images/Meals.svg';
 import drinks from '../images/Drinks.svg';
 
